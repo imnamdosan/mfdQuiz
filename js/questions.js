3102944041853e88fdaab89a0d498b11723bb028
Nam Dosan <107978194+imnamdosan@users.noreply.github.com>
2023-02-25 16:27:18 +1100

    Initial commit

---
+++ js/questions.js
@@ -0,0 +1,62 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which sperm is normal?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "../mfdQuiz/Images/Abnormal/1.jpg",
+      "../mfdQuiz/Images/Abnormal/1.jpg"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Which sperm is normal?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which sperm is normal?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which sperm is normal?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Which sperm is normal?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
